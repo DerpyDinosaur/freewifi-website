913ff47af3f30994e2764cb73bd6ef8766803b89
Adam <33823436+DerpyDinosaur@users.noreply.github.com>
2021-02-20 16:00:52 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Bank Login">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<title>Universal Bank Login</title>
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1>The Universal Bank</h1>
+	</header>
+	<main>
+		<h2 class="loginHeader">Login Page</h2>
+		<form>
+			<h4>Username</h4>
+			<input class="form-control" type="text" name="username">
+			<h4>Password</h4>
+			<input class="form-control" type="text" name="password">
+			<button class='btn btn-success' type='submit'>Sign Up</button>
+		</form>
+	</main>
+	<footer>
+		
+	</footer>
+</body>
+</html>
